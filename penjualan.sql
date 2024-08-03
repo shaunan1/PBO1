b35bacc0048148b6e3f50b97d62a050865fba077
shaunan1 <163667442+shaunan1@users.noreply.github.com>
2024-08-03 23:25:29 +0700

    Add files via upload

---
+++ penjualan.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 03, 2024 at 06:25 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `ID_Barang` varchar(10) NOT NULL,
+  `Nama_Barang` varchar(50) NOT NULL,
+  `Jenis` varchar(20) DEFAULT NULL,
+  `Ukuran` varchar(5) DEFAULT NULL,
+  `HargaBeli` decimal(10,2) DEFAULT NULL,
+  `HargaJual` decimal(10,2) DEFAULT NULL,
+  `Stok` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`ID_Barang`, `Nama_Barang`, `Jenis`, `Ukuran`, `HargaBeli`, `HargaJual`, `Stok`, `created_at`, `updated_at`) VALUES
+('BR001', '1', 'Baju', 'S', 1.00, 1.00, 2, '2024-07-19 00:15:46', '2024-08-03 15:24:15'),
+('BR002', '1', 'Baju', 'S', 1.00, 1.00, 1, '2024-08-03 15:33:49', '2024-08-03 15:33:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `NoFaktur` varchar(20) NOT NULL,
+  `Tanggal` varchar(20) NOT NULL,
+  `ID_Customer` varchar(10) NOT NULL,
+  `TotalBeli` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualanrinci`
+--
+
+CREATE TABLE `penjualanrinci` (
+  `NoFaktur` varchar(20) NOT NULL,
+  `ID_Barang` varchar(10) NOT NULL,
+  `Nama_Barang` varchar(50) NOT NULL,
+  `Jumlah` int(11) NOT NULL,
+  `Harga` double NOT NULL,
+  `Total` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `penjualanrinci`
+--
+
+INSERT INTO `penjualanrinci` (`NoFaktur`, `ID_Barang`, `Nama_Barang`, `Jumlah`, `Harga`, `Total`) VALUES
+('TR0001', 'PK001', 'Pakaian', 10, 5000, 50000),
+('TR0001', 'BD001', 'Bed Cover', 5, 10000, 50000),
+('TR0001', 'KPT001', 'Karpet', 10, 20000, 200000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`ID_Barang`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`NoFaktur`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
